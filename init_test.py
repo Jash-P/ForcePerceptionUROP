439a169f5de207c6e6c6837111cd589fb98181bc
Jash-P <parimijash@gmail.com>
2025-06-27 16:57:13 +0100

    Added waypoints into arm motion

--- init_test.py
+++ init_test.py
@@ -1,9 +1,26 @@
 import rtde_control
 
+# NOTE: add 0.4 to the z-axis value on pendant
+
+def main():
+    velocity = 0.5
+    acceleration = 0.5
+    blend_1 = 0.0
+    blend_2 = 0.02
+    blend_3 = 0.0
+    path_pose1 = [-0.143, -0.435, 0.20, -0.001, 3.12, 0.04, velocity, acceleration, blend_1]
+    path_pose2 = [-0.143, -0.435, 0.30, -0.001, 3.12, 0.04, velocity, acceleration, blend_2]
+    path_pose3 = [0.250, -0.610, 0.170, 1.327, -2.865, -0.022, velocity, acceleration, blend_3]
+    path = [path_pose1, path_pose2, path_pose3]
+
+    # Send a linear path with blending in between - (currently uses separate script)
+    rtde_c.moveL(path)
+    rtde_c.stopScript()
+
 try:
     rtde_c = rtde_control.RTDEControlInterface("169.254.150.50")
     print("Connected")
-    rtde_c.moveJ([0.256, -0.597, -0.233, 0.797, -2.333, -1.452], 0.5, 0.3)
+    main()
 except Exception as e:
     print(f"Exception occurred: {e}")
 finally:
